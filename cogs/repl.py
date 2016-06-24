255b8b4714e5b4936bd31fb7c79a8a72fac227bc
Michael Van Buren <mike64221@gmail.com>
2016-06-24 13:06:24 -0700

    add Danny's debug/eval command to repl cog

--- cogs/repl.py
+++ cogs/repl.py
@@ -107,5 +107,35 @@ class REPL:
             except discord.HTTPException as e:
                 await self.bot.send_message(msg.channel, 'Unexpected error: `{}`'.format(e))
 
+    @commands.command(pass_context=True, hidden=True, name='eval')
+    @checks.is_owner()
+    async def debug(self, ctx, *, code : str):
+        """Evaluates code."""
+        code = code.strip('` ')
+        python = '```py\n{}\n```'
+        result = None
+
+        env = {
+            'bot': self.bot,
+            'ctx': ctx,
+            'message': ctx.message,
+            'server': ctx.message.server,
+            'channel': ctx.message.channel,
+            'author': ctx.message.author
+        }
+
+        env.update(globals())
+
+        try:
+            result = eval(code, env)
+            if inspect.isawaitable(result):
+                result = await result
+        except Exception as e:
+            await self.bot.say(python.format(type(e).__name__ + ': ' + str(e)))
+            return
+
+        await self.bot.say(python.format(result))
+
+
 def setup(bot):
     bot.add_cog(REPL(bot))
