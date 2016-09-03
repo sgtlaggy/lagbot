85a7bbe7cb44921b17f4e6af131564754173a5df
Michael Van Buren <mike64221@gmail.com>
2016-09-02 21:37:44 -0700

    add optional change_nickname permission to join link

--- cogs/admin.py
+++ cogs/admin.py
@@ -56,6 +56,7 @@ class Management:
         perm.send_messages = True
         perm.manage_messages = True
         perm.embed_links = True
+        perm.change_nickname = True
         url = discord.utils.oauth_url(self.bot.client_id, permissions=perm)
 
         message = []
