f57cce8262b60d903c0f527aefb227f641008be4
Michael Van Buren <mike64221@gmail.com>
2016-09-20 21:18:20 -0700

    typo

--- bot.py
+++ bot.py
@@ -150,7 +150,7 @@ if __name__ == '__main__':
                  **config)
     bot.add_cog(CogManagement(bot))
 
-    for cog in inital_cogs:
+    for cog in initial_cogs:
         try:
             bot.load_extension(cog)
         except Exception as e:
