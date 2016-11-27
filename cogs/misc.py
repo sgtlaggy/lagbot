d57f945058201535602c3e80f929bc391c2967d6
Michael Van Buren <mike64221@gmail.com>
2016-11-27 11:32:30 -0800

    use embed for !info, fix and upgrade fancy_time

--- cogs/misc.py
+++ cogs/misc.py
@@ -18,13 +18,18 @@ CLOCKS = (clocks[-1], *clocks[:-1])
 UNILINK = "http://www.fileformat.info/info/unicode/char/{}/index.htm"
 
 
-def fancy_time(orig_time):
-    diff = datetime.utcnow() - orig_time
+def fancy_time(orig_time, utc=False):
+    diff = datetime.datetime.utcnow() - orig_time
     nice = ''
     if diff.days >= 365:
-        nice += str(diff.days // 365) + ' years, '
-    nice += str(diff.days % 365) + ' days ago'
-    nice += ' ({} UTC)'.format(orig_time)
+        years = diff.days // 365
+        nice += '{} year{}, '.format(years, plural(years))
+    days = diff.days % 365
+    nice += '{} day{} ago'.format(days, plural(days))
+    if utc:
+        nice += ' ({} UTC)'.format(orig_time)
+    else:
+        nice += ' ({})'.format(orig_time.strftime('%Y-%m-%d'))
     return nice
 
 
@@ -171,29 +176,21 @@ class Misc:
                     title, win_score, plural(win_score), '\n'.join(winners)))
 
     @commands.command(pass_context=True, no_pm=True)
-    async def info(self, ctx, *, member: discord.Member):
+    async def info(self, ctx, *, member: discord.Member = None):
         """Display information of specific user."""
-        message = []
-
+        member = member or ctx.message.author
         roles = [role.name.replace('@', '@\u200b')
                  for role in member.roles
                  if role.name != '@everyone']
-
-        message.append('```ocaml')
-        lines = [
-            ('Name', member.name),
-            ('Tag', member.discriminator),
-            ('ID', member.id),
-            ('Joined Server', fancy_time(member.joined_at)),
-            ('Joined Discord', fancy_time(member.created_at)),
-            ('Roles', ', '.join(roles)),
-            ('Avatar', member.avatar_url)]
-        width = max(len(k) for k, v in lines) + 1
-        for line in lines:
-            message.append('{0:<{width}}: {1}'.format(*line, width=width))
-        message.append('```')
-        message = '\n'.join(message)
-        await self.bot.say(message)
+        embed = discord.Embed(colour=member.colour)
+        embed.add_field(name='Name', value=member.name)
+        embed.add_field(name='Tag', value=member.discriminator)
+        embed.add_field(name='ID', value=member.id)
+        embed.add_field(name='Joined Server', value=fancy_time(member.joined_at)[::-1].replace(' ', '\n', 1)[::-1])
+        embed.add_field(name='Joined Discord', value=fancy_time(member.created_at)[::-1].replace(' ', '\n', 1)[::-1])
+        embed.add_field(name='Roles', value=', '.join(roles))
+        embed.set_image(url=member.avatar_url)
+        await self.bot.say(embed=embed)
 
     @commands.command()
     async def charinfo(self, *, chars):
