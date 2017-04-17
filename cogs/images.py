b9988cf1ccc5e16a7b82706836e25229edb381e5
Michael Van Buren <mike64221@gmail.com>
2017-04-17 09:45:40 -0700

    switch from TheCatAPI to random.cat

--- cogs/images.py
+++ cogs/images.py
@@ -1,31 +1,16 @@
-from xml.etree import ElementTree as XMLTree
 import datetime
-import asyncio
 
 from discord.ext import commands
 import discord
-import aiohttp
 import asyncpg
 
-from utils.checks import bot_config_attr, need_db
+from utils.checks import need_db
 from utils.errors import NotFound
 from utils.utils import between, integer
 from cogs.base import BaseCog
 
 
 FACTS = 'http://catfacts-api.appspot.com/api/facts?number={count}'
-
-HOME = 'http://thecatapi.com/'
-HOME_BY_ID = HOME + '?id={image_id}'
-GET = HOME + 'api/images/get?api_key={api_key}&format=xml{category}&sub_id={sub_id}'
-REPORT = HOME + 'api/images/report?api_key={api_key}&sub_id={sub_id}&image_id={image_id}{reason}'
-VOTE = HOME + 'api/images/vote?api_key={api_key}&sub_id={sub_id}&image_id={image_id}&score={score}'
-GET_VOTES = HOME + 'api/images/getvotes?api_key={api_key}&sub_id={sub_id}'
-FAVE = HOME + 'api/images/favourite?api_key={api_key}&sub_id={sub_id}&image_id={image_id}&action={act}'
-GET_FAVES = HOME + 'api/images/getfavourites?api_key={api_key}&sub_id={sub_id}'
-
-CATEGORIES = {'hats', 'space', 'funny', 'sunglasses', 'boxes',
-              'caturday', 'ties', 'dream', 'sinks', 'clothes'}
 
 
 def xkcd_date(data):
@@ -117,65 +102,20 @@ class Images(BaseCog):
             raise NotFound('No cat fact available.')
         return j['facts']
 
-    async def fetch_cat(self, url, **format_args):
-        url = url.format(api_key=self.bot.config['catapi_key'],
-                         reason=format_args.pop('reason', ''),
-                         **format_args)
-        status, data = await self.bot.request(url, 'text')
+    async def fetch_cat(self):
+        status, data = await self.bot.request('http://random.cat/meow')
         if status != 200:
             raise NotFound('Could not get cat.')
-        return data
-
-    async def try_cat_image(self, url):
-        if url is None:
-            return False
-        try:
-            resp = await self.bot.request(url, 'read')
-        except aiohttp.ClientOSError:  # DNS name not resolved
-            return False
-        else:
-            return resp.status == 200
+        return data['file']
 
     @commands.group(invoke_without_command=True)
-    @bot_config_attr('catapi_key')
-    async def cat(self, ctx, category=''):
-        """Get a random cat image.
-
-        [category] can be one of:
-            hats, space, funny, sunglasses,
-            boxes, caturday, ties, dream,
-            sinks, clothes
-
-        Within 30 seconds of the image being posted anyone can react with :poop: to report the image.
-        Please only report images that do not contain cats.
-        """
-        if category in CATEGORIES:
-            category = '&category=' + category
-        elif category:
-            await ctx.send('Invalid category. Try one of: ' + ', '.join(CATEGORIES))
-        sub_id = ctx.author.id
+    async def cat(self, ctx):
+        """Get a random cat image."""
         with ctx.typing():
             try:
-                while True:
-                    try:
-                        x = await self.fetch_cat(GET, category=category, sub_id=sub_id)
-                    except aiohttp.ClientResponseError:
-                        continue
-                    try:
-                        image_root = XMLTree.fromstring(x).find('data').find('images').find('image')
-                    except XMLTree.ParseError:
-                        continue
-                    image_url = image_root.find('url').text
-                    image_id = image_root.find('id').text
-                    if await self.try_cat_image(image_url):
-                        break
-                    else:
-                        await self.fetch_cat(REPORT,
-                                             sub_id=sub_id,
-                                             image_id=image_id,
-                                             reason='DNS name not resolved')
+                image_url = await self.fetch_cat()
             except NotFound as e:
-                await ctx.send(str(e))
+                await ctx.send(e)
                 return
 
             try:
@@ -183,30 +123,9 @@ class Images(BaseCog):
             except NotFound as e:
                 fact = ''
 
-            embed = discord.Embed(title=image_id,
-                                  url=HOME_BY_ID.format(image_id=image_id),
-                                  description=fact or None,
-                                  color=discord.Color.red())
+            embed = discord.Embed(description=fact or None)
             embed.set_image(url=image_url)
-            embed.set_footer(text='Add reaction \N{PILE OF POO} to report this image.')
-            msg = await ctx.send(embed=embed)
-
-        def report_check(reaction, user):
-            return reaction.message.id == msg.id and reaction.emoji == '\N{PILE OF POO}'
-
-        try:
-            await self.bot.wait_for('reaction_add', check=report_check, timeout=30)
-        except asyncio.TimeoutError:
-            pass
-        else:
-            await self.fetch_cat(REPORT, sub_id=sub_id, image_id=image_id)
-        finally:
-            embed.set_footer()
-            embed.color = discord.Color.default()
-            try:
-                await msg.edit(embed=embed)
-            except discord.NotFound:
-                pass
+        await ctx.send(embed=embed)
 
     @cat.command(name='facts', aliases=['fact'])
     async def cat_facts(self, ctx, count: integer = 1):
--- docs/Images.md
+++ docs/Images.md
@@ -4,9 +4,6 @@
 * [xkcd](#xkcd)
 * [Cat](#cat)
 * [- Facts](#facts)
-* [- Ratings](#ratings)
-* [- Rerate](#rerate)
-* [- Faves](#faves)
 
 ---
 
@@ -22,28 +19,9 @@ Usage:
 
 ## Cat
 #### Get a random cat image and fact.
-Available categories:
-
-* hats
-* space
-* funny
-* sunglasses
-* boxes
-* caturday
-* ties
-* dream
-* sinks
-* clothes
-
 Usage:
 
-    !cat [category]     : Get random image, optionally with a category.
-
-After the bot replies with an image, for the next 30 seconds, anyone can rate the image, favorite it, or report it.
-
-To rate the image, reply "X/10" where X is any number 1-10. Any number less than 1 will be changed to 1 and any number greater than 10 will be changed to 10.
-To favorite the image, reply "fave", "favorite", or "favourite".
-To report the image, reply "report" or "report and your reason here".
+    !cat                : Get random image, optionally with a category.
 
 ---
 
@@ -59,37 +37,3 @@ Usage:
 
     !cat facts          : Get 1 cat fact.
     !cat facts 5        : Get 5 cat facts.
-
----
-
-### Ratings
-#### Get a list of images you've rated.
-Note this is in a code block to avoid image spam.
-
-Usage:
-
-    !cat ratings
-
----
-
-### Rerate
-#### Re-rate an image you've previously rated.
-Usage:
-
-    !cat rerate <image_id> <new_score>
-
-    !cat rerate bC24 7
-    !cat rerate bC24 7/10
-
----
-
-### Faves
-#### See all the images you've favorited.
-This can be called with `!cat faves`, `!cat favorites`, or `!cat favourites`.
-
-Usage:
-
-    !cat faves [to_remove]
-
-    !cat faves              : List favorited images.
-    !cat faves bC24         : Unfavorite image "bC24"
