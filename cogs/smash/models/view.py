0032b01f4683cab06bfdef15caedf5fee9252821
sgtlaggy <8661717+sgtlaggy@users.noreply.github.com>
2021-07-04 20:26:37 -0700

    add select menus more programatically

--- cogs/smash/models/view.py
+++ cogs/smash/models/view.py
@@ -1,6 +1,7 @@
 import functools
 import random
 
+from discord.ext import commands
 from discord import ui
 import discord
 
@@ -8,59 +9,57 @@ import discord
 from .game import Game, EndReason
 from .fighter import Fighter, FakeFighter
 from .modes import MODES
+from .errors import SmashError
 
 
 FIGHTER_OPTIONS = [discord.SelectOption(label=fighter.name, emoji=fighter.emoji) for fighter in Fighter.all()]
 FIGHTER_OPTIONS = list(discord.utils.as_chunks(FIGHTER_OPTIONS, 22))
-PLACEHOLDERS = [str(group[0]) for group in FIGHTER_OPTIONS]
 RANDOM_FIGHTER = discord.SelectOption(label='random', emoji='\N{GAME DIE}')
-UNPLAYED_ROUND = discord.SelectOption(label='sit out', emoji='\N{CHAIR}')
+UNPLAYED_ROUND = discord.SelectOption(label='sit out', value='-', emoji='\N{CHAIR}')
+FIGHTER_OPTIONS[-1] += [RANDOM_FIGHTER, UNPLAYED_ROUND]
+
+
+class FakeContext:
+    def __init__(self, view: ui.View, interaction: discord.Interaction):
+        self.interaction = interaction
+        self.author = interaction.user
+        _, self.player = view.get_models(interaction)
+
+    async def send(self, *args, delete_after=None, **kwargs):
+        return await self.interaction.response.send_message(*args, **kwargs, ephemeral=True)
+
+
+class GameSelect(ui.Select):
+    def get_command(self, command: str) -> commands.Command:
+        return self.view.game.context.bot.get_command(command)
+
+
+class PickFighterMenu(GameSelect):
+    def __init__(self, *args, options: list[discord.SelectOption], **kwargs):
+        super().__init__(placeholder=str(options[0]), options=options)
+
+    async def callback(self, interaction: discord.Interaction):
+        cmd = self.get_command('pick')
+        try:
+            await cmd(FakeContext(self.view, interaction), fighter=self.values[0])
+        except SmashError as err:
+            await interaction.response.send_message(str(err), ephemeral=True)
 
 
 class GameView(ui.View):
     def __init__(self, game: Game, *args, **kwargs):
-        super().__init__(*args, **kwargs)
         self.game = game
-        
+        super().__init__(*args, **kwargs)
+        for row, options in enumerate(FIGHTER_OPTIONS):
+            self.add_item(PickFighterMenu(options=options, row=row))
+
     def get_models(self, interaction: discord.Interaction):
         return self.game, self.game.players[interaction.user]
-        
+
     async def interaction_check(self, interaction: discord.Interaction):
         return interaction.user in self.game.players
-    
-    async def pick_fighter(self, select: ui.Select, interaction: discord.Interaction):
-        game, player = self.get_models(interaction)
-        value = select.values[0]
-        
-        if value == UNPLAYED_ROUND.value:
-            fighter = FakeFighter('-')
-        else:
-            fighter = Fighter.get_exact(value)
 
-        allowed = self.game.mode.pick_check(player, fighter)
-        if allowed:
-            self.game.players[interaction.user].play(fighter)
-            await game.update()
-        else:
-            await interaction.response.send_message(allowed, ephemeral=True)
-    
-    @ui.select(placeholder=PLACEHOLDERS[0], options=FIGHTER_OPTIONS[0])
-    async def pick_1(self, select, interaction):
-        await self.pick_fighter(select, interaction)
-    
-    @ui.select(placeholder=PLACEHOLDERS[1], options=FIGHTER_OPTIONS[1])
-    async def pick_2(self, select, interaction):
-        await self.pick_fighter(select, interaction)
-    
-    @ui.select(placeholder=PLACEHOLDERS[2], options=FIGHTER_OPTIONS[2])
-    async def pick_3(self, select, interaction):
-        await self.pick_fighter(select, interaction)
-    
-    @ui.select(placeholder=PLACEHOLDERS[3], options=FIGHTER_OPTIONS[3] + [RANDOM_FIGHTER, UNPLAYED_ROUND])
-    async def pick_4(self, select, interaction):
-        await self.pick_fighter(select, interaction)
-            
-    @ui.button(label='Win', emoji='\N{PARTY POPPER}', style=discord.ButtonStyle.success)
+    @ui.button(label='Win', emoji='\N{PARTY POPPER}', style=discord.ButtonStyle.success, row=4)
     async def win_latest_round(self, button: ui.Button, interaction: discord.Interaction):
         game, player = self.get_models(interaction)
         if player.win(player.current_round):
@@ -69,7 +68,7 @@ class GameView(ui.View):
             else:
                 await game.update()
 
-    @ui.button(label='Undo', emoji='\N{LEFTWARDS ARROW WITH HOOK}\ufe0f', style=discord.ButtonStyle.primary)
+    @ui.button(label='Undo', emoji='\N{LEFTWARDS ARROW WITH HOOK}\ufe0f', style=discord.ButtonStyle.primary, row=4)
     async def undo_latest_action(self, button: ui.Button, interaction: discord.Interaction):
         game, player = self.get_models(interaction)
         if player.undo():
